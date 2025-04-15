e05e8cd3567f26a914ad0f91f3e0ec5ba895921e
EduardoLR10 <dudulr10@gmail.com>
2025-04-15 00:53:39 -0300

    Update flake + add direnv + tools

---
+++ tools.nix
@@ -0,0 +1,32 @@
+{
+    benevides = [
+        {
+            name = "vim";
+            publisher = "vscodevim";
+            version = "1.29.0";
+            sha256 = "sha256-J3V8SZJZ2LSL8QfdoOtHI1ZDmGDVerTRYP4NZU17SeQ=";
+        }
+    ];
+    marinho = [
+        {
+            name = "dance";
+            publisher = "gregoire";
+            version = "0.5.15001";
+            sha256 = "sha256-gGTpeOQeIQj2ObyC6504+lzLFUS35RNw5z2/isPRpyM=";
+        }
+    ];
+    lemos = [
+        {
+            name = "emacs-mcx";
+            publisher = "tuttieee";
+            version = "0.65.1";
+            sha256 = "sha256-YHdwjd8o/QtNae/7VLgouw2v5Ork+acchfxHr4Ycz/E=";
+        }
+        {
+            name = "Theme-Brogrammer";
+            publisher = "gerane";
+            version = "0.1.3";
+            sha256 = "sha256-ssTNeaGYqKRzQZhemLywJyYPilbE+sLHs9qwsqO48dk=";
+        }
+    ];
+}
